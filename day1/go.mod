d39e0de29466eb9981afb334546f00633a6dd3f1
Attila Fabian <attila.fabian@ibm.com>
2020-12-02 09:25:00 +0100

    day1 solved

---
+++ day1/go.mod
@@ -0,0 +1,3 @@
+module github.com/attiss/advent-of-code-2020/day1
+
+go 1.15
---
+++ day1/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+const (
+	inputFile = "input.txt"
+	target    = 2020
+)
+
+func main() {
+	input, err := readInputFromFile(inputFile)
+	if err != nil {
+		log.Fatalf("failed to get input: %v", err)
+	}
+
+	sort.Ints(input)
+
+	var foundTarget bool
+	var result int
+	var steps int
+	for i := 0; i < len(input); i++ {
+		steps++
+
+		if foundTarget {
+			break
+		}
+
+		for j := len(input) - 1; j >= 0; j-- {
+			if (input[i] + input[j]) == target {
+				foundTarget = true
+				result = input[i] * input[j]
+				break
+			}
+		}
+	}
+
+	if foundTarget {
+		log.Printf("successfully found target (in %d steps), calculated result: %d", steps, result)
+	} else {
+		log.Println("failed to find target")
+	}
+}
+
+func readInputFromFile(filePath string) (input []int, err error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var parsedInt int
+		parsedInt, err = strconv.Atoi(scanner.Text())
+		input = append(input, parsedInt)
+	}
+
+	if err = scanner.Err(); err != nil {
+		return
+	}
+
+	return
+}
